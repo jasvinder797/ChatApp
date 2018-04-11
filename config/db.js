85f41408b763c0e47224ce0b7e060a7cc8353836
Jassi797 <jasvinder797@gmail.com>
2018-04-11 10:44:17 +0530

    update1.3

--- config/db.js
+++ config/db.js
@@ -2,5 +2,4 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //var connection = mongoose.connect('mongodb://localhost:27017/dbchat'); 
-var connection = mongoose.connect('mongodb://jasvinder797:powerpack@ds237379.mlab.com:37379/chatdb'); 
 module.exports = connection;
